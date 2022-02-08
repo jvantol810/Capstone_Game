7a69b8104c0f3d00ca4e3689eb8dc8f4f05fa521
jvantol810 <63761346+jvantol810@users.noreply.github.com>
2022-02-08 17:00:46 -0500

    Created a static LevelSettings script which contains the MapData that RoomGen and aStarGrid scripts get the map size from

--- Assets/Scenes/JasonsStuff/astar/AStarGrid.cs
+++ Assets/Scenes/JasonsStuff/astar/AStarGrid.cs
@@ -7,8 +7,7 @@ public class AStarGrid : MonoBehaviour
 {
     //public Tilemap walkableMap;
     public Tilemap map;
-    [SerializeField]
-    public MapData mapData;
+
     [SerializeField]
     public WorldTileRow[] tileRow;
 
@@ -20,62 +19,33 @@ public class AStarGrid : MonoBehaviour
     private void Start()
     {
         //CreateWorldTiles();
-        tileRow = new WorldTileRow[mapData.width];
+        tileRow = new WorldTileRow[LevelSettings.MapData.width];
         //Go through tile array and create each column of height mapHeight
         for(int i = 0; i < tileRow.Length; i++)
         {
-            tileRow[i].tileColumn = new WorldTile[mapData.height];
+            tileRow[i].tileColumn = new WorldTile[LevelSettings.MapData.height];
         }
     }
 
+    //Add a new WorldTile object to the two dimensional tileArray. This does not check if a tile already exists here, so it will replace it.
     public void AddTile(WorldTile newTile)
     {
         tileRow[newTile.gridX].tileColumn[newTile.gridY] = newTile;
         Debug.Log("Tile added at (x: " + newTile.gridX + ", y: " + newTile.gridY + ") -- Walkable: " + newTile.walkable);
     }
 
-    public void ReplaceTileAt(Vector2Int originalTilePosition, WorldTile newTile)
+    public int GetDistance(WorldTile tileA, WorldTile tileB)
     {
-        tileRow[originalTilePosition.x].tileColumn[newTile.gridY] = newTile;
-        Debug.Log("Tile at (x: " + originalTilePosition.x + ", y: " + originalTilePosition.y + ") replaced with new tile.");
-    }
+        int distanceX = Mathf.Abs(tileA.gridX - tileB.gridX);
+        int distanceY = Mathf.Abs(tileA.gridY - tileB.gridY);
 
-    //Create a 2D array of tiles from the map
-    public /*WorldTile[,]*/ void CreateWorldTiles()
-    {
-        WorldTile[,] tileArray = new WorldTile[mapData.height, mapData.height];
-
-        for (int i = 0; i < mapData.height; i++)
+        if(distanceX > distanceY)
         {
-            for (int j = 0; j < mapData.width; j++)
-            {
-                
-                //Get the tile found at the current point in the tilemap
-                //TileBase tile = walkableMap.GetTile(new Vector3Int(j, i, 1));
-                
-                bool tileWalkable;
-                //If there's a tile on the unwalkable tilemap at this point, set the tile to be unwalkable
-                if (map.GetTile(new Vector3Int(j, i, 1)) != null) {
-                    tileWalkable = false;
-                }
-                else
-                {
-                    tileWalkable = true;
-                }
-                //Construct a WorldTile class and add it to the TileArray
-                WorldTile newTile = new WorldTile(tileWalkable, j, i);
-                tileArray[j, i] = newTile;
-            }
-        }//end of outer loop
-
-        ////Add the newTile to the tileArray
-        ////tileArray[x, y] = newTile;
-        //Debug.Log("Tile found at coordinates -- X: " + x + "  Y: " + y);
-
-        //Print contents of tileArray
-        foreach (WorldTile tile in tileArray)
+            return 14 * distanceY + 10 * (distanceX - distanceY);
+        }
+        else
         {
-            Debug.Log("Tile found at (x: " + tile.gridX + ", y: " + tile.gridY + ") -- Walkable: " + tile.walkable);
+            return 14 * distanceX + 10 * (distanceY - distanceX);
         }
     }
 }
