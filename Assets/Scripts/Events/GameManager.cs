a2d0a3af8af3d325ad8d06509b570ffe7c74d5de
Stonie212 <tstoneburg212@gmail.com>
2022-04-10 15:49:02 -0400

    messed with game manager

--- Assets/Scripts/Events/GameManager.cs
+++ Assets/Scripts/Events/GameManager.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
-    
+
     public static GameManager Instance
     {
         get
@@ -15,14 +16,20 @@ public class GameManager : MonoBehaviour
                 Debug.LogError("Game Manager is NULL");
             return _instance;
         }
-            
+
     }
     private void Awake()
     {
         _instance = this;
+        GameEvents.OnEnterTeleporter.AddListener(ChangeLevel);
     }
-    public void NewScene()
+    public void NewGame()
     {
+        SceneManager.LoadScene(1);
+       //DontDestroyOnLoad();
+    }
+    
+    public void ChangeLevel() { 
         SceneManager.LoadScene(1);
     }
 }
